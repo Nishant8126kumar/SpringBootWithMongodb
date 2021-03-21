8fa91714651609933fb1f0bceb860235a9503685
Nishant8126kumar <nk1761698@gmail.com>
2021-03-21 13:24:53 +0530

    initial commit in spring boot with mongodb

---
+++ src/test/kotlin/com/example/vehicleManager/VehicleManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.vehicleManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VehicleManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
